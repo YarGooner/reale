7ae851f130662cecd1a56c9ac5079b6a983f8cd0
YarGooner <yargunt@gmail.com>
2023-01-16 19:11:08 +0300

    123

--- environments/dev/htdocs/admin/index.php
+++ environments/dev/htdocs/admin/index.php
@@ -1,18 +1,21 @@
 <?php
-
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../../common/config/bootstrap.php';
-require __DIR__ . '/../config/bootstrap.php';
+$path = '/../../';
+
+require __DIR__ . $path.'vendor/autoload.php';
+require __DIR__ . $path.'vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . $path.'common/config/bootstrap.php';
+require __DIR__ . $path.'admin/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
-    require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php'
+    require __DIR__ . $path.'common/config/main.php',
+    require __DIR__ . $path.'common/config/main-local.php',
+    require __DIR__ . $path.'admin/config/main.php',
+    require __DIR__ . $path.'admin/config/main-local.php'
 );
 
+error_reporting(E_ALL & ~E_NOTICE);
 (new yii\web\Application($config))->run();
+
